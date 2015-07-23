415e35d4f42a7c99e8dac1b74c1e63a31ccc2dfe
kanekotic <alvarojosepl@gmail.com>
2015-07-23 21:42:08 +0200

    solved typo, and improved settings control

--- Soft.Hati.PlayYouTube/Soft.Hati.PlayYouTube.App/Services/Options/OptionUserControl.Designer.cs
+++ Soft.Hati.PlayYouTube/Soft.Hati.PlayYouTube.App/Services/Options/OptionUserControl.Designer.cs
@@ -30,36 +30,49 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.SearchEngineCB = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 30);
+            this.label1.Location = new System.Drawing.Point(18, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.Size = new System.Drawing.Size(77, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Search Engine";
+            this.label1.Text = "Filter Level";
             // 
             // SearchEngineCB
             // 
             this.SearchEngineCB.FormattingEnabled = true;
-            this.SearchEngineCB.Location = new System.Drawing.Point(125, 27);
+            this.SearchEngineCB.Location = new System.Drawing.Point(125, 29);
             this.SearchEngineCB.Name = "SearchEngineCB";
-            this.SearchEngineCB.Size = new System.Drawing.Size(409, 24);
+            this.SearchEngineCB.Size = new System.Drawing.Size(496, 24);
             this.SearchEngineCB.TabIndex = 1;
             this.SearchEngineCB.SelectionChangeCommitted += new System.EventHandler(this.SearchEngineCB_SelectionChangeCommitted);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SearchEngineCB);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(627, 72);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Query";
             // 
             // OptionUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.SearchEngineCB);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "OptionUserControl";
             this.Size = new System.Drawing.Size(633, 379);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -67,5 +80,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox SearchEngineCB;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
